0b316483640302e20bd6db257cd8c8e8a74a5fb4
kostiantyn <firstpickpudge@gmail.com>
2023-02-16 21:14:53 +0200

    added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { DishCategoryModule } from './dish-category/dish-category.module';
-import { IngredientModule } from './ingredient/ingredient.module';
-import { DishModule } from './dish/dish.module';
-import { FoodModule } from './food/food.module';
-import { OrderModule } from './order/order.module';
-import { CustomModule } from './custom/custom.module';
-import { EventModule } from './event/event.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { DishCategoryModule } from "./dish-category/dish-category.module";
+import { IngredientModule } from "./ingredient/ingredient.module";
+import { DishModule } from "./dish/dish.module";
+import { FoodModule } from "./food/food.module";
+import { OrderModule } from "./order/order.module";
+import { CustomModule } from "./custom/custom.module";
+import { EventModule } from "./event/event.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [AuthModule, DishCategoryModule, IngredientModule, DishModule, FoodModule, OrderModule, CustomModule, EventModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    DishCategoryModule,
+    IngredientModule,
+    DishModule,
+    FoodModule,
+    OrderModule,
+    CustomModule,
+    EventModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
