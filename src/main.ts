99ec701f4f7d970c6341f1bf7eec771b0b2ce3de
kostiantyn <firstpickpudge@gmail.com>
2023-03-19 14:59:32 +0200

    added cors options and categories logos

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  const corsOptions: CorsOptions = {
+    origin: "http://localhost:3001",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+  app.setGlobalPrefix("api");
   await app.listen(3000);
 }
 bootstrap();
