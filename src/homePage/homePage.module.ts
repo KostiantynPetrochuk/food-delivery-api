fdce0be731322eb67db805a3bebc77690274385a
kostiantyn <firstpickpudge@gmail.com>
2023-04-01 19:54:33 +0300

    changed home page data route

--- src/homePage/homePage.module.ts
+++ src/homePage/homePage.module.ts
@@ -2,7 +2,8 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { HomePageController } from "./homePage.controller";
 import { Dish, DishSchema } from "../dish/dish.schema/dish.schema";
-import { Event, EventSchema } from "src/event/event.schema/event.schema";
+import { Event, EventSchema } from "../event/event.schema/event.schema";
+import { Ingredient, IngredientSchema } from "../ingredient/ingredient.schema/ingredient.schema";
 import { HomePageService } from "./homePage.service";
 
 @Module({
@@ -10,6 +11,7 @@ import { HomePageService } from "./homePage.service";
     MongooseModule.forFeature([
       { name: Dish.name, schema: DishSchema },
       { name: Event.name, schema: EventSchema },
+      { name: Ingredient.name, schema: IngredientSchema },
     ]),
   ],
   controllers: [HomePageController],
--- src/homePage/homePage.service.ts
+++ src/homePage/homePage.service.ts
@@ -4,20 +4,17 @@ import { Model } from "mongoose";
 import { ObjectId } from "mongodb";
 import { Dish, DishDocument } from "../dish/dish.schema/dish.schema";
 import { Event, EventDocument } from "src/event/event.schema/event.schema";
+import { Ingredient, IngredientDocument } from "src/ingredient/ingredient.schema/ingredient.schema";
 
 @Injectable()
 export class HomePageService {
   constructor(
     @InjectModel(Dish.name) private dishModel: Model<DishDocument>,
     @InjectModel(Event.name) private eventModel: Model<EventDocument>,
+    @InjectModel(Ingredient.name) private ingredientModel: Model<IngredientDocument>,
   ) {}
 
-  async getHomeDishesAndEvents(): Promise<{
-    novelties: Dish[];
-    ramens: Dish[];
-    pizzas: Dish[];
-    latestEvents: Event[];
-  }> {
+  async getHomeDishesAndEvents() {
     const novelties = await this.dishModel
       .find({ isNovelty: true })
       .sort({ _id: -1 })
@@ -46,8 +43,43 @@ export class HomePageService {
       ])
       .exec();
 
+    const ramensIds = ramens.map((ramen) => ramen._id);
+
+    const ramensIngredients = await this.ingredientModel
+      .find({ dish: { $in: ramensIds } })
+      .populate("food", "name")
+      .exec();
+
+    const ramensWithIngredients = ramens.map((ramen) => {
+      const ramenIngredients = ramensIngredients
+        .filter((ingredient) => ingredient.dish.toString() === ramen._id.toString())
+        .map((ingredient) => ingredient.food.name);
+
+      return { ...ramen, ingredients: ramenIngredients };
+    });
+
+    const pizzasIds = pizzas.map((pizza) => pizza._id);
+
+    const pizzasIngredients = await this.ingredientModel
+      .find({ dish: { $in: pizzasIds } })
+      .populate("food", "name")
+      .exec();
+
+    const pizzasWithIngredients = pizzas.map((pizza) => {
+      const pizzaIngredients = pizzasIngredients
+        .filter((ingredient) => ingredient.dish.toString() === pizza._id.toString())
+        .map((ingredient) => ingredient.food.name);
+
+      return { ...pizza, ingredients: pizzaIngredients };
+    });
+
     const latestEvents = await this.eventModel.find().sort({ _id: -1 }).limit(4).exec();
 
-    return { novelties, ramens, pizzas, latestEvents };
+    return {
+      novelties,
+      ramen: ramensWithIngredients,
+      pizza: pizzasWithIngredients,
+      latestEvents,
+    };
   }
 }
