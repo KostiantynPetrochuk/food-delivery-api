9b427847ddcc9d5d1b05dcc2ffd2538e7aad000c
KostiantynPetrochuk <firstpickpudge@gmail.com>
2023-06-29 21:53:11 +0300

    update frontend route

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: "https://food-delivery-app-reactjs.vercel.app/",
+    origin: "https://food-delivery-app-reactjs.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     optionsSuccessStatus: 200,
     credentials: true,
